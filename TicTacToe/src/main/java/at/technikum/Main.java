9054fe178fe93af504b5cea75aaad1d05c752ada
Lexan <alexander@helmstedt.at>
2025-07-02 12:13:00 +0200

    Finalize working version of TicTacToe game

--- TicTacToe/src/main/java/at/technikum/Main.java
+++ TicTacToe/src/main/java/at/technikum/Main.java
@@ -2,7 +2,7 @@ package main.java.at.technikum;
 
 public class Main {
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
+        main.java.at.technikum.TicTacToe game = new main.java.at.technikum.TicTacToe();
         game.start();
     }
 }
