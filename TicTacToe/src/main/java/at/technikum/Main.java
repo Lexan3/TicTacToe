3fbc2fcf4d87fcc2e070ffb680ddfe81adec9795
Lexan <alexander@helmstedt.at>
2025-07-02 11:47:59 +0200

    Save current changes before merging player

--- TicTacToe/src/main/java/at/technikum/Main.java
+++ TicTacToe/src/main/java/at/technikum/Main.java
@@ -1,4 +1,4 @@
-package at.technikum;
+package main.java.at.technikum;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
